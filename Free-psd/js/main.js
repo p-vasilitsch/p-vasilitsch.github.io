f23d0b2aa177073a16393672f75bcd9377a7d352
p-vasilitsch <72552655+p-vasilitsch@users.noreply.github.com>
2020-10-28 12:05:06 +0800

    Add files via upload

---
+++ Free-psd/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.slider__inner').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next">1</button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev">2</button>',
+    })
+
+
+
+
+})

d4c029b6454cca702e54cd9b0d6aee4d4bafbcce
p-vasilitsch <72552655+p-vasilitsch@users.noreply.github.com>
2020-11-07 15:32:58 +0800

    Update main.js

--- Free-psd/js/main.js
+++ Free-psd/js/main.js
@@ -4,9 +4,9 @@ $(function () {
         nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
         prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
         infinite: false
-    })
+    });
 
 
 
 
-})
+});

14926b43eeaebfe77eb3eff7cbd8dbb57a9bf970
p-vasilitsch <72552655+p-vasilitsch@users.noreply.github.com>
2020-12-07 23:17:05 +0800

    Update main.js

--- BaikalSee/js/main.js
+++ BaikalSee/js/main.js
@@ -2,5 +2,6 @@ $(function () {
     $('.fairy-tail__slider').slick({
         prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/2.png"></button>',
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/1.png"></button>',
+        autoplay: true
     });
 });

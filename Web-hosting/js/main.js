252c2c19782721d7ecadaa5303911e019af53a82
p-vasilitsch <72552655+p-vasilitsch@users.noreply.github.com>
2021-01-07 15:49:04 +0800

    Update main.js

--- Web-hosting/js/main.js
+++ Web-hosting/js/main.js
@@ -1,3 +1,12 @@
-$(function(){
-
+$(function () {
+    $('.reviews-slider').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        arrows: false,
+        dots: true
+    });
+    $('.btn__menu').on('click', function () {
+        $('.menu__list').slideToggle();
+    });
 });
